acc4aad6da5bb54e83a700bcf2c8d0b9034eabc5
Mo-sk <studio.galoyan@gmail.com>
2024-09-24 17:03:58 +0200

    feat: Homepage

--- pages/[uid].vue
+++ pages/[uid].vue
@@ -7,9 +7,10 @@ const { data: page } = useAsyncData(route.params.uid as string, () =>
   prismic.client.getByUID('page', route.params.uid as string)
 )
 
-useHead({
-  title: prismic.asText(page.value?.data.title)
-})
+useSeoMeta({
+  title: page.value?.data.meta_title ?? undefined,
+  description: page.value?.data.meta_description ?? undefined,
+  ogImage: prismic.asImageSrc(page.value?.data.meta_image) ?? undefined
 </script>
 
 
