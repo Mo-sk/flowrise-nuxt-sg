019de5e9f41b7a90434b54c8f2c60dc6e7f0c756
Mo-sk <studio.galoyan@gmail.com>
2024-09-24 17:09:31 +0200

    update

--- pages/[uid].vue
+++ pages/[uid].vue
@@ -11,6 +11,7 @@ useSeoMeta({
   title: page.value?.data.meta_title ?? undefined,
   description: page.value?.data.meta_description ?? undefined,
   ogImage: prismic.asImageSrc(page.value?.data.meta_image) ?? undefined
+})
 </script>
 
 
