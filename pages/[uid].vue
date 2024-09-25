97a9cc4e1cf9355bd71d70bdc8741efbdd5d88af
Mo-sk <studio.galoyan@gmail.com>
2024-09-25 14:44:20 +0200

    feat: setup uid

--- pages/[uid].vue
+++ pages/[uid].vue
@@ -4,7 +4,14 @@ import { components } from '~/slices'
 const prismic = usePrismic()
 const route = useRoute()
 const { data: page } = useAsyncData(route.params.uid as string, () =>
-  prismic.client.getByUID('page', route.params.uid as string)
+  prismic.client.getByUID('page', route.params.uid as string, {
+    fetchLinks: [
+      'testimonial.quote',
+      'testimonial.avatar',
+      'testimonial.name',
+      'testimonial.job_title'
+    ]
+  })
 )
 
 useSeoMeta({
